987579105303dd88b2c9ae691bea694e39805df8
Michael Ernst <mernst@cs.washington.edu>
2018-07-11 07:20:35 -0700

    Initial revision

---
+++ README.md
@@ -0,0 +1,69 @@
+# require-javadoc
+
+A simple Javadoc doclet that requires that a Javadoc comment be present on
+every Java element (class, method, and field).
+
+It is important that every Java element be documented, but the programmer can use judgment about how extensive the comment needs to be.  Therefore, 
+This makes no requirement
+about the Javadoc comment; for example, it does not require the existence
+of any Javadoc tags such as `@param`, `@return`, etc.
+
+
+## Use
+
+To run a doclet, invoke `javadoc` with the `-doclet` command-line argument:
+
+```
+javadoc -doclet org.plumelib.javadoc.RequireJavadoc -docletpath require-javadoc-all.jar ...
+```
+
+Add `-private` to check all Java elements, not just public ones.
+
+
+## Gradle
+
+To add a `requireJavadoc` target to your Gradle buildfile `build.gradle`, add the following to it:
+
+```
+configurations {
+  requireJavadoc
+}
+dependencies {
+  compile group: 'org.plumelib', name: 'require-javadoc', version: '0.0.7'
+}
+task requireJavadoc(type: Javadoc) {
+  description = 'Ensures that all Java elementt have Javadoc documentation.'
+  destinationDir.deleteDir()
+  source = sourceSets.main.allJava
+  classpath = project.sourceSets.main.compileClasspath
+  // options.memberLevel = JavadocMemberLevel.PRIVATE
+  options.docletpath = project.sourceSets.main.compileClasspath as List
+  options.doclet = "org.plumelib.javadoc.RequireJavadoc"
+  // options.addStringOption('skip', 'ClassNotToCheck|OtherClass')
+}
+```
+
+
+## Alternatives
+
+If you want to require all Javadoc tags to be present, use the Javadoc tool itself (possibly adjust the [`-Xdoclint` argument](https://docs.oracle.com/javase/8/docs/technotes/tools/unix/javadoc.html#BEJEFABE)):
+```javadoc -Xwerror -Xdoclint:all```
+This project makes fewer requirements than Javadoc's linting, which requires all tags.
+Requiring all tags can be pedantic and tedious; this project produces fewer warnings, and
+therefore adopting it is easier for an existing project.
+
+Here is how to enable Javadoc's stricter checking, when you are ready to do so:
+```
+// Turn Javadoc warnings into errors.
+javadoc {
+  options.addStringOption('Xwerror', '-Xdoclint:all')
+  options.addStringOption('private', '-quiet')
+}
+```
+
+Another alternative is Checkstyle.  Unlike Javadoc, Checkstyle is configurable to produce
+the same warnings as this class.
+Checkstyle is heavyweight to run and configure:  configuring it requires multiple files.
+As of July 2018, Checkstyle is nondeterministic:  it gives different results for file `A.java` depending on what other files are being checked, and it crashes on correct code (for example, see https://github.com/checkstyle/checkstyle/issues/5989).
+By contrast to Checkstyle, 
+this project is smaller and actually works.
---
+++ src/main/java/org/plumelib/javadoc/RequireJavadoc.java
@@ -0,0 +1,292 @@
+package org.plumelib.javadoc;
+
+import com.sun.javadoc.AnnotationDesc;
+import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.ConstructorDoc;
+import com.sun.javadoc.Doc;
+import com.sun.javadoc.DocErrorReporter;
+import com.sun.javadoc.FieldDoc;
+import com.sun.javadoc.MethodDoc;
+import com.sun.javadoc.PackageDoc;
+import com.sun.javadoc.RootDoc;
+import com.sun.javadoc.SourcePosition;
+import com.sun.tools.doclets.standard.Standard;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * A Javadoc doclet that issues an error for any package, class, method, or field that lacks a
+ * Javadoc comment.
+ */
+// This extends the standard doclet so that it takes the standard command-line arguments, which
+// makes it easier to use in an existing build or with a build system.
+public class RequireJavadoc extends Standard {
+
+  /** All the errors this doclet will report. */
+  List<Undocumented> errors = new ArrayList<>();
+
+  /**
+   * All packages that have been seen and checked so far. This helps prevent issuing multiple
+   * warnings about a given package.
+   */
+  Set<PackageDoc> packages = new HashSet<>();
+
+  /** If true, print filenames relative to working directory. If false, print absolute paths. */
+  static boolean relativePaths = false;
+
+  static Pattern skipped = null;
+
+  private static final /*@Format({})*/ String USAGE =
+      "Provided by RequireJavadoc doclet:%n"
+          + "-skip <classname>      Don't report problems in the given class%n"
+          + "-relative              Report relative rather than absolute filenames%n"
+          + "See the documentation for more details.%n";
+
+  /** Creates a new RequireJavadoc instance. */
+  RequireJavadoc() {}
+
+  /**
+   * The entry point to this doclet. Requires documentation of all Java packages, classes, methods,
+   * and fields.
+   *
+   * <p>{@inheritDoc}
+   */
+  public static boolean start(RootDoc root) {
+    RequireJavadoc doclet = new RequireJavadoc();
+    for (ClassDoc cd : root.classes()) {
+      doclet.processClass(cd);
+    }
+
+    Path currentPath = Paths.get("").toAbsolutePath();
+    // Report errors in lexicographic order by file, and (more importantly) within a file in the
+    // same order as they appear in the file.
+    Collections.sort(doclet.errors);
+    for (Undocumented error : doclet.errors) {
+      // Making the pathname relative shortens it, but can confuse tools.
+      SourcePosition position = error.position;
+      if (position == null) {
+        System.err.printf("missing documentation for %s%n", error.name);
+      } else {
+        File file = position.file();
+        Path path = (relativePaths ? currentPath.relativize(file.toPath()) : file.toPath());
+        System.err.printf(
+            "%s:%d: missing documentation for %s%n", file, error.position.line(), error.name);
+      }
+    }
+
+    return doclet.errors.isEmpty();
+  }
+
+  /**
+   * Require documentation of the given class and its package.
+   *
+   * @param cd the class to require documentation of
+   */
+  private void processClass(ClassDoc cd) {
+    if (skipped != null
+        && (skipped.matcher(cd.name()).find()
+            || skipped.matcher(cd.qualifiedName()).find()
+            || skipped.matcher(cd.position().file().toString()).find())) {
+      return;
+    }
+    requireCommentText(cd);
+    PackageDoc pd = cd.containingPackage();
+    // TODO: This does not work: it does not require documentation for packages.
+    if (packages.add(pd)) {
+      requireCommentText(pd);
+    }
+    for (ConstructorDoc constructorDoc : cd.constructors()) {
+      // Don't require documentation of synthetic constructors, which don't appear in the source
+      // code.
+      if (!constructorDoc.isSynthetic() && !isSyntheticForNestedConstructor(constructorDoc)) {
+        requireCommentText(constructorDoc);
+      }
+    }
+    for (FieldDoc fd : cd.enumConstants()) {
+      requireCommentText(fd);
+    }
+    for (FieldDoc fd : cd.fields()) {
+      // Don't require documentation for `long serialVersionUID`.
+      if (!(fd.name().equals("serialVersionUID") && (fd.type().toString().equals("long")))) {
+        requireCommentText(fd);
+      }
+    }
+    for (ClassDoc icd : cd.innerClasses()) {
+      requireCommentText(icd);
+    }
+    for (MethodDoc md : cd.methods()) {
+      // Don't require documentation of overriding methods
+      if (!isOverride(md)) {
+        requireCommentText(md);
+      }
+    }
+  }
+
+  /**
+   * Return true if this method is annotated with {@code @Override}.
+   *
+   * @param md the method to check for an {@code @Override} annotation
+   * @return true if this method is annotated with {@code @Override}
+   */
+  private boolean isOverride(MethodDoc md) {
+    for (AnnotationDesc anno : md.annotations()) {
+      // TODO: A String comparison is a bit gross.
+      if (anno.toString().equals("@java.lang.Override")) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Return true if this method is a synthetic default constructor. The <a
+   * href="https://docs.oracle.com/javase/8/docs/jdk/api/javadoc/doclet/com/sun/javadoc/MemberDoc.html">isSynthetic</a>
+   * documentation says "Returns true if this member was synthesized by the compiler.", but it
+   * returns false for a synthesized default constructor for a nested class. This handles that case.
+   *
+   * @param cd the constructor to check
+   * @return true if {@code cd} is a synthetic constructor
+   */
+  private boolean isSyntheticForNestedConstructor(ConstructorDoc cd) {
+    SourcePosition constructorPosition = cd.position();
+    SourcePosition classPosition = cd.containingClass().position();
+    return classPosition.file().equals(constructorPosition.file())
+        && classPosition.line() == constructorPosition.line();
+  }
+
+  /**
+   * Require a documentation string for the given Java element. If there is no documentation, this
+   * doclet will print a warning message and will return an error status.
+   *
+   * @param d any Java element
+   */
+  private void requireCommentText(Doc d) {
+    if (skipped != null && skipped.matcher(d.name()).find()) {
+      return;
+    }
+    String text = d.getRawCommentText();
+    if (text.isEmpty()) {
+      errors.add(new Undocumented(d.name(), d.position()));
+    }
+  }
+
+  /** An undocumented identifier and the file and line number where it appears. */
+  private class Undocumented implements Comparable<Undocumented> {
+    /** The identifier that is not documented. */
+    final String name;
+    /** The position (file and line number) where it is defined. */
+    final SourcePosition position;
+    /**
+     * Create a new Undocumented.
+     *
+     * @param name the identifier that is not documented
+     * @param position where it is defined
+     */
+    public Undocumented(String name, SourcePosition position) {
+      this.name = name;
+      this.position = position;
+    }
+    /** Not consistent with equals. (It ignores the name.) */
+    public int compareTo(Undocumented other) {
+      if (other.position == null) {
+        return -1;
+      }
+      if (this.position == null) {
+        return 1;
+      }
+      int cmp;
+      File thisFile = this.position.file();
+      File otherFile = other.position.file();
+      if (thisFile == null) {
+        return 1;
+      }
+      cmp = thisFile.compareTo(otherFile);
+      if (cmp != 0) {
+        return cmp;
+      }
+      cmp = this.position.line() - other.position.line();
+      if (cmp != 0) {
+        return cmp;
+      }
+      cmp = this.position.column() - other.position.column();
+      return cmp;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Undocumented(%s, %s)", name, position);
+    }
+  }
+
+  /**
+   * Given a command-line option of this doclet, returns the number of arguments you must specify on
+   * the command line for the given option. Returns 0 if the argument is not recognized. This method
+   * is automatically invoked by Javadoc.
+   *
+   * @param option the command-line option
+   * @return the number of command-line arguments needed when using the option
+   * @see <a
+   *     href="https://docs.oracle.com/javase/8/docs/technotes/guides/javadoc/doclet/overview.html">Doclet
+   *     overview</a>
+   */
+  public static int optionLength(String option) {
+    switch (option) {
+      case "-relative":
+        return 1;
+      case "-skip":
+        return 2;
+      default:
+        return Standard.optionLength(option);
+    }
+  }
+
+  /**
+   * Tests the validity of command-line arguments passed to this doclet. Returns true if the option
+   * usage is valid, and false otherwise. This method is automatically invoked by Javadoc.
+   *
+   * <p>Also sets fields from the command-line arguments.
+   *
+   * @param options the command-line options to be checked: an array of 1- or 2-element arrays,
+   *     where the length depends on {@link #optionLength} applied to the first element
+   * @param reporter where to report errors
+   * @return true iff the command-line options are valid
+   * @see <a
+   *     href="https://docs.oracle.com/javase/8/docs/technotes/guides/javadoc/doclet/overview.html">Doclet
+   *     overview</a>
+   */
+  @SuppressWarnings("index") // dependent: os[1] is legal when optionLength(os[0])==2
+  public static boolean validOptions(String[] /*@MinLen(1)*/[] options, DocErrorReporter reporter) {
+    List<String[]> remaining = new ArrayList<>();
+    for (int oi = 0; oi < options.length; oi++) {
+      String[] os = options[oi];
+      String opt = os[0].toLowerCase();
+      switch (opt) {
+        case "-relative":
+          relativePaths = true;
+          break;
+        case "-skip":
+          try {
+            skipped = Pattern.compile(os[1]);
+          } catch (PatternSyntaxException e) {
+            System.err.printf("Bad regex \"%s\"%n", os[1]);
+          }
+          break;
+        case "-help":
+          System.out.printf(USAGE);
+          return false;
+        default:
+          remaining.add(os);
+          break;
+      }
+    }
+    return Standard.validOptions((String[][]) remaining.toArray(new String[0][]), reporter);
+  }
+}
