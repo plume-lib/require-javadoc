6a95984448d1f39b8db79652cd20abdfaef8ee04
Michael Ernst <mernst@cs.washington.edu>
2025-01-29 19:08:08 -0800

    Run tests in CI

--- src/test/run-tests.sh
+++ src/test/run-tests.sh
@@ -1,6 +1,8 @@
-Tests are not yet automated. :-(
+#!/bin/sh
 
-The diff output should be empty:
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+cd ${SCRIPTDIR}
 
 (cd ../.. && ./gradlew assemble) && sleep .1 && java -cp ../../build/libs/require-javadoc-1.0.9-all.jar org.plumelib.javadoc.RequireJavadoc --relative --dont-require-trivial-properties --dont-require-noarg-constructor > out.txt 
 
